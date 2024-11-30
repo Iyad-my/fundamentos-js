d92dfc36ecba37efe32264610ef2b0df19333317
iyad <imoucharrafie@gmail.com>
2024-11-30 16:28:55 -0400

    condicionales encadenados

--- main.js
+++ main.js
@@ -1,68 +1,95 @@
-// // Variables
-// let MyName;
+// // // Variables
+// // let MyName;
 
-// MyName = "Hi";
+// // MyName = "Hi";
 
-// let MyAge = 15;
+// // let MyAge = 15;
 
-// alert(MyName);
+// // alert(MyName);
 
-// let MyName = "josefino2",
-//   MyAge = 15,
-//   MyMessage = "hello";
+// // let MyName = "josefino2",
+// //   MyAge = 15,
+// //   MyMessage = "hello";
 
-//   alert(MyName);
-//   alert(MyAge);
-//   alert(MyMessage);
+// //   alert(MyName);
+// //   alert(MyAge);
+// //   alert(MyMessage);
 
-// constantes 
-const MyBirthDay = "oct-06-1998";
+// // constantes 
+// const MyBirthDay = "oct-06-1998";
 
-// alert(MyBirthDay);
-let MyAge2 = 33;
-let IsActive  = true;
-let IsBlue = false;
+// // alert(MyBirthDay);
+// let MyAge2 = 33;
+// let IsActive  = true;
+// let IsBlue = false;
 
-let MyHeight = null;
-let MyWeight2 = undefined;
+// let MyHeight = null;
+// let MyWeight2 = undefined;
 
-// ingreso de argumentos
-let year = prompt('año actual:')
-// alert(year);
+// // ingreso de argumentos
+// let year = prompt('año actual:')
+// // alert(year);
 
-// confirm
-let isBoss = confirm("Eres el jefe");
-// alert(isBoss);
+// // confirm
+// let isBoss = confirm("Eres el jefe");
+// // alert(isBoss);
 
-// // operadores matematicos
-// let num1 = 10;
-// // let num2 = 20;
+// // // operadores matematicos
+// // let num1 = 10;
+// // // let num2 = 20;
 
-// // //suma
-// // alert( Num1 + Num2 );
+// // // //suma
+// // // alert( Num1 + Num2 );
 
-// // //resta 
-// // alert( Num1 - Num2 );
+// // // //resta 
+// // // alert( Num1 - Num2 );
 
-// // // multiplicacion
-// // alert( Num1 * Num2 );
+// // // // multiplicacion
+// // // alert( Num1 * Num2 );
 
-// // // division
-// // alert( Num1 / Num2 );
+// // // // division
+// // // alert( Num1 / Num2 );
 
-// // incremento
-// let Num3 = 2;
-// Num3++;
-// alert(Num3);
+// // // incremento
+// // let Num3 = 2;
+// // Num3++;
+// // alert(Num3);
 
+// // operadores de comparacion
+// // alert( 2 > 1 );
+// // alert( 2 < 1 );
+// // alert( 2 == 1 );
+// // alert( 2 != 1 );
 
-// operadores de comparacion
-alert( 2 > 1 );
-alert( 2 < 1 );
-alert( 2 == 1 );
-alert( 2 != 1 );
+// // condicionales
+// // let year = prompt("ingrese el año actual:")
 
-// condicionales
-  
+// // if ( year == 2024 ) {
+// //     alert("Muy Bien");
+// //     alert("eres great");
 
+// // else
+// let year = prompt("ingrese el año actual:")
+
+// if ( year == 2024 ) {
+//     alert("Muy Bien");
+//     alert("eres great");
+// } else {
+//     alert("No es correcto");
+// }
+
+// condicionales encadenados 
+
+let year = prompt("ingrese el año actual:")
+
+if ( year == 2024 ) {
+    alert("Muy Bien");
+    alert("eres great");
+} else if ( year >2024 ) {
+    alert("Muy alto");
+} else if ( year < 2024 ) {
+    alert("Muy bajo");
+} else {
+    alert("No es correcto");
+}
 
