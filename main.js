ec1c2850d36c723558dac55d3978c138a43cc6ff
iyad <imoucharrafie@gmail.com>
2024-11-30 16:04:26 -0400

    first comit

---
+++ main.js
@@ -0,0 +1,68 @@
+// // Variables
+// let MyName;
+
+// MyName = "Hi";
+
+// let MyAge = 15;
+
+// alert(MyName);
+
+// let MyName = "josefino2",
+//   MyAge = 15,
+//   MyMessage = "hello";
+
+//   alert(MyName);
+//   alert(MyAge);
+//   alert(MyMessage);
+
+// constantes 
+const MyBirthDay = "oct-06-1998";
+
+// alert(MyBirthDay);
+let MyAge2 = 33;
+let IsActive  = true;
+let IsBlue = false;
+
+let MyHeight = null;
+let MyWeight2 = undefined;
+
+// ingreso de argumentos
+let year = prompt('año actual:')
+// alert(year);
+
+// confirm
+let isBoss = confirm("Eres el jefe");
+// alert(isBoss);
+
+// // operadores matematicos
+// let num1 = 10;
+// // let num2 = 20;
+
+// // //suma
+// // alert( Num1 + Num2 );
+
+// // //resta 
+// // alert( Num1 - Num2 );
+
+// // // multiplicacion
+// // alert( Num1 * Num2 );
+
+// // // division
+// // alert( Num1 / Num2 );
+
+// // incremento
+// let Num3 = 2;
+// Num3++;
+// alert(Num3);
+
+
+// operadores de comparacion
+alert( 2 > 1 );
+alert( 2 < 1 );
+alert( 2 == 1 );
+alert( 2 != 1 );
+
+// condicionales
+  
+
+
